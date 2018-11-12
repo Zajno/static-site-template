0792b89a00518704aad06bbb33f3453cefa8c144
Ivan Dudinov <ivan@zajno.com>
2018-11-12 11:37:10 +0200

    Upgraded babel

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
-import '../styles/main.sass';
-
 import 'polyfills';
 import logger from 'logger';
+
+import '../styles/main.sass';
 
 import './pages';
 import App from 'core/app';
--- app/js/polyfills.js
+++ app/js/polyfills.js
@@ -1,3 +1,4 @@
+import '@babel/polyfill';
 
 // NodeList.forEach for IE
 if (window.NodeList && !NodeList.prototype.forEach) {
